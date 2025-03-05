04691d6d4aac264e0034d646309dbfc87ce6c5d6
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-05 09:25:21 +0300

    First Commit

---
+++ 1Kotlin_Introduction/hello.kt
@@ -0,0 +1,6 @@
+// Hello World Program
+fun main(args: Array<String>) {
+    println("Hello, World!");//Calls System.out.println() internally.
+}
+/** Multi-Line Comment. */
+//Single Line Comment. Ends at the end of a line.
---
+++ 2Kotlin_Fundamentals/Variables.kt
@@ -0,0 +1,7 @@
+fun main(args : Array<String>) {
+  val range: Byte = 112
+  println("$range")
+
+  // The code below gives error. Why?
+  // val range1: Byte = 200
+}
