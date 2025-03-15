c76d8f05d4324e80febb23277c0b86ee8d752ce5
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-15 22:01:48 +0300

    Commit for the 2nd folder.

--- 2Kotlin_Fundamentals/Variables.kt
+++ 2Kotlin_Fundamentals/Variables.kt
@@ -2,6 +2,9 @@ fun main(args : Array<String>) {
   val range: Byte = 112
   println("$range")
 
+  val unreadCount = 5
+  val readCount = 100
+  println("You have ${unreadCount + readCount} total messages in your inbox.")
   // The code below gives error. Why?
   // val range1: Byte = 200
 }
