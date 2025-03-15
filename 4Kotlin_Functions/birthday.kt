7b1d163b473a74d6837b7784441cbd0e8808e53d
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-15 22:55:12 +0300

    Commit for 4th folder

--- 4Kotlin_Functions/birthday.kt
+++ 4Kotlin_Functions/birthday.kt
@@ -25,3 +25,20 @@ fun birthdayGreeting3(name: String): String {
   val ageGreeting = "You are now 20 years old!"
   return "$nameGreeting\n$ageGreeting"
 }
+
+
+/*
+Note: Although you often find them used interchangeably, 
+a parameter and an argument aren't the same thing. 
+When you define a function, you define the parameters 
+that are to be passed to it when the function is called. 
+When you call a function, you pass arguments for the parameters. 
+Parameters are the variables accessible to the function, such as 
+a name variable, while arguments are the actual values that you pass, 
+such as the "Rover" string.
+
+
+Unlike in some languages, such as Java, where a function can change 
+the value passed into a parameter, parameters in Kotlin are immutable. 
+You cannot reassign the value of a parameter from within the function body.
+ */
