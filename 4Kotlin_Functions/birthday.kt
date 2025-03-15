ac59ac5f4033bb782c30c9a4a3d396997414d61a
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-15 22:33:24 +0300

    Commit for 4th folder

--- 4Kotlin_Functions/birthday.kt
+++ 4Kotlin_Functions/birthday.kt
@@ -1,8 +1,12 @@
 fun main() {
   val greeting = birthdayGreeting()
-  println(greeting)
+  println("$greeting \n")
 
+  //println(birthdayGreeting());
   birthdayGreeting2()
+
+  val greeting3 = birthdayGreeting3("Yamlak")
+  println("\n $greeting3")
 }
 
 fun birthdayGreeting2() {
@@ -13,5 +17,11 @@ fun birthdayGreeting2() {
 fun birthdayGreeting(): String {
   val nameGreeting = "Happy Birthday, Rover!"
   val ageGreeting = "You are now 5 years old!"
+  return "$nameGreeting\n$ageGreeting"
+}
+
+fun birthdayGreeting3(name: String): String {
+  val nameGreeting = "Happy Birthday, $name!"
+  val ageGreeting = "You are now 20 years old!"
   return "$nameGreeting\n$ageGreeting"
 }
